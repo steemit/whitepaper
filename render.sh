df340e48b517bffd87596e9581dfb17345a76663
thrize <thrize@tutanota.de>
2017-03-23 08:27:02 -0300

    initial set up of LaTex whitepaper document, with some of introduction, table of contents, footers

---
+++ render.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# TODO use LaTeX CLI to render whitepaper.tex to PDF
+
+# Will need to set environment variable with local installation folder
+# On Mac this is /Library/TeX/texbin/
+
+# should use build/ folder and copy result to root
